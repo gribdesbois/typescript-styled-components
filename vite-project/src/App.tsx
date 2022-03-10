f7a752cfd5a3c1bd25533096aa6e4271110b0d43
gribdesbois <gribdesbois@gmail.com>
2022-03-10 15:12:02 +0100

    done

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,11 +3,13 @@ import { useState } from 'react'
 import './App.css'
 import Menubar from './components/Menubar'
 import Navbar from './components/Navbar'
+import PageBody from './components/PageBody'
 
 const App: React.FC = () => (
   <>
     <Menubar />
     <Navbar />
+    <PageBody />
   </>
 )
 
---
+++ vite-project/src/components/PageBody/index.tsx
@@ -0,0 +1,78 @@
+import {
+  KeyboardArrowLeft,
+  Star,
+  StarHalf,
+  KeyboardArrowDown,
+} from '@material-ui/icons'
+import {
+  Container,
+  LeftContainer,
+  RightContainer,
+  Wrapper,
+  Image,
+  Text,
+  Paragraph,
+  ProductContainer,
+  ImageContainer,
+  SearchResultDiv,
+  IconWrapper,
+  BoldText,
+} from './styles'
+import { departmentList, productDetails } from './PageInfo'
+
+const Menubar: React.FC = () => (
+  <Container>
+    <LeftContainer>
+      <BoldText>Department</BoldText>
+      {departmentList.map((item, index) => (
+        <Paragraph key={index}>
+          {' '}
+          <KeyboardArrowLeft />
+          {item}
+        </Paragraph>
+      ))}
+    </LeftContainer>
+    <RightContainer>
+      <SearchResultDiv>
+        <BoldText>1-12 of over 2,000 results for</BoldText>
+        <BoldText color="#c45500"> All Departments</BoldText>
+      </SearchResultDiv>
+
+      <ProductContainer>
+        {productDetails.map((item, index) => (
+          <Wrapper flexDirection="column" key={index}>
+            <ImageContainer>
+              <Image src={item.src} />
+            </ImageContainer>
+            <Text>{item.name}</Text>
+            <Text fontSize=".8em" color="grey">
+              {item.by}
+            </Text>
+
+            {/* stars */}
+            <Wrapper margin=".3em 0 0 0" alignItems="center">
+              <IconWrapper>
+                <Star />
+                <Star />
+                <Star />
+                <Star />
+                <StarHalf />
+              </IconWrapper>
+
+              <KeyboardArrowDown />
+              <Text color="blue">{item.starcount}</Text>
+            </Wrapper>
+
+            <Wrapper alignItems="center" margin=".3em 0 .3em 0">
+              <Text fontSize="1.3em">{item.price}</Text>
+              <Text color="grey">{item.shipping}</Text>
+            </Wrapper>
+            <Text>{item.available}</Text>
+          </Wrapper>
+        ))}
+      </ProductContainer>
+    </RightContainer>
+  </Container>
+)
+
+export default Menubar
