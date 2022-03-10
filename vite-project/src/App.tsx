c802a5b6d2668d1b3967be791bad4c19df00fbf3
gribdesbois <gribdesbois@gmail.com>
2022-03-10 12:12:30 +0100

    assets / Container style

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,6 +1,29 @@
 import { useState } from 'react'
+
+import {
+  ShoppingCartOutlined,
+  SearchOutlined,
+  ArrowDropDown,
+  RoomOutlined,
+} from '@material-ui/icons'
+import {
+  Container,
+  Logo,
+  Flag,
+  Text,
+  Wrapper,
+  SearchBox,
+  Select,
+  SearchIconWrapper,
+} from './styles'
+import logo from './assets/logo.png'
+import flag from './assets/flag.png'
 import './App.css'
 
-const App: React.FC = () => <></>
+const App: React.FC = () => (
+  <>
+    <Container></Container>
+  </>
+)
 
 export default App
